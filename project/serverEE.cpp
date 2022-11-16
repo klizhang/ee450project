d461410a39aac870aa5d3889c806a4b82b293c1d
Kevin <klizhang@usc.edu>
2022-11-16 00:32:25 -0800

    all servers working and server m is good too

--- project/serverEE.cpp
+++ project/serverEE.cpp
@@ -18,6 +18,8 @@
 #include <bits/stdc++.h>
 
 #define LOCALHOST "127.0.0.1" // Host address
+#define EEPORT "23606"
+#define MAXBUFLEN 100
 
 using namespace std;
 
@@ -43,13 +45,11 @@ int main() {
             getline (myfile,line);
             //https://www.geeksforgeeks.org/program-to-parse-a-comma-separated-string-in-c/#:~:text=Get%20the%20substring%20if%20the,and%20stored%20in%20the%20vector
             stringstream ss(line);
-            // string substr;
             while (ss.good()) {
                 string substr;
                 getline(ss, substr, ',');
                 v.push_back(substr);
             }
-            
         }
         myfile.close();
     }
@@ -69,11 +69,112 @@ int main() {
         eeList.push_back(ee);
         
     }
-    for(int i=0;i<eeList.size();i++)
-    {
-        cout<<eeList[i].courseID<<endl;
-        cout<<eeList[i].prof<<endl;
+    //SOURCE from beej
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    char buf[MAXBUFLEN];
+    socklen_t addr_len;
+    //char s[INET_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    //hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(LOCALHOST, EEPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
     }
 
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("listener: socket");
+            continue;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("The ServerEE is up and running using UDP on port %s \n",EEPORT);
+    while(1){
+        addr_len = sizeof their_addr;
+        if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+            (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+            perror("recvfrom");
+            exit(1);
+        }
+        buf[numbytes] = '\0';
+        cout<<buf<<endl;
+        vector<string> v;
+        stringstream ss(buf);
+        while (ss.good()) {
+            string substr;
+            getline(ss, substr, ',');
+            v.push_back(substr);
+        }
+        string courseNum = v[0];
+        string category = v[1];
+        printf("The ServerEE received a request from the Main Server about the %s of %s.\n",category.c_str(),courseNum.c_str());
+        string responseEE = "Wrong input";
+        for(size_t i=0;i<eeList.size();i++)
+        {    
+            if(strcmp(courseNum.c_str(),eeList[i].courseID.c_str())==0){
+                if(strcmp(category.c_str(),"Credit")==0){
+                    responseEE = to_string(eeList[i].numCredits);
+                    printf("The course information has been found: The %s of %s is %s \n",category.c_str(),courseNum.c_str(),responseEE.c_str());
+                }
+                else if(strcmp(category.c_str(),"Professor")==0){
+                    responseEE = eeList[i].prof;
+                    printf("The course information has been found: The %s of %s is %s \n",category.c_str(),courseNum.c_str(),responseEE.c_str());
+                }
+                else if(strcmp(category.c_str(),"Days")==0){
+                    responseEE = eeList[i].days;
+                    printf("The course information has been found: The %s of %s is %s \n",category.c_str(),courseNum.c_str(),responseEE.c_str());
+                }
+                else if(strcmp(category.c_str(),"CourseName")==0){
+                    responseEE = eeList[i].courseName;
+                    printf("The course information has been found: The %s of %s is %s \n",category.c_str(),courseNum.c_str(),responseEE.c_str());
+                }
+                else{
+                    responseEE = "Wrong category";
+                    printf("Wrong category: %s, the only valid categories are (Credit / Professor / Days / CourseName)",category.c_str());
+                }
+                break;
+            }
+        }
+        if(strcmp(responseEE.c_str(),"Wrong input")==0){
+            printf("Didn’t find the course: %s \n",courseNum.c_str());
+        }
+ 
+        int n = responseEE.length();
+        char char_array_EE[n + 1];
+        strcpy(char_array_EE, responseEE.c_str());
+        if ((numbytes = sendto(sockfd, char_array_EE, strlen(char_array_EE), 
+          0,(struct sockaddr *)&their_addr, addr_len)) == -1) {
+		    perror("serverEE: sendto");
+		    exit(1);
+	    }
+        cout<<"The ServerEE finished sending the response to the Main Server."<<endl;
+        
+    }
+    close(sockfd);
     return 0;
 }
--- project/serverM.cpp
+++ project/serverM.cpp
@@ -19,6 +19,8 @@
 
 #define LOCALHOST "127.0.0.1" // Host address
 #define SERVERPORT "21606"	// the port users will be connecting to
+#define CSPORT "22606"
+#define EEPORT "23606"
 #define MAXBUFLEN 100
 
 using namespace std;
@@ -156,16 +158,135 @@ int main() {
     }
     if(count==maxTries){
         printf("Authentication Failed for 3 attempts. Client will shut down. \n");
+        freeaddrinfo(servinfo);
+	    close(sockfd);
     }
     else{
         printf("%s received the result of authentication using TCP over port %s.",username,SERVERPORT);
         printf("Authentication is successful \n");
+        freeaddrinfo(servinfo);
+	    close(sockfd);
+        if ((rv = getaddrinfo(LOCALHOST, CSPORT, &hints, &servinfo)) != 0) {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+            return 1;
+        }
+        // loop through all the results and make a socket
+        for(p = servinfo; p != NULL; p = p->ai_next) {
+            if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                    p->ai_protocol)) == -1) {
+                perror("talker: socket");
+                continue;
+            }
+
+            break;
+        }
+        if (p == NULL) {
+            fprintf(stderr, "talker: failed to create socket\n");
+            return 2;
+        }
+
+        int sockfdEE;
+        struct addrinfo hintsEE, *servinfoEE, *pEE;
+        int rvEE;
+        int numbytesEE;
+
+        memset(&hintsEE, 0, sizeof hintsEE);
+        hintsEE.ai_family = AF_INET; // set to AF_INET to use IPv4
+        hintsEE.ai_socktype = SOCK_DGRAM;
+
+        if ((rvEE = getaddrinfo(LOCALHOST, EEPORT, &hintsEE, &servinfoEE)) != 0) {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rvEE));
+            return 1;
+        }
+        // loop through all the results and make a socket
+        for(pEE = servinfoEE; pEE != NULL; pEE = pEE->ai_next) {
+            if ((sockfdEE = socket(pEE->ai_family, pEE->ai_socktype,
+                    pEE->ai_protocol)) == -1) {
+                perror("talker: socket");
+                continue;
+            }
+
+            break;
+        }
+        if (pEE == NULL) {
+            fprintf(stderr, "talker: failed to create socket\n");
+            return 2;
+        }
+
+        //HERE GOES COURSES REQUESTS
+        while(1){
+            cout<<"Please enter the course code to query: ";
+            char courseNum[50];
+            cin.getline(courseNum,50); //take the message as input
+            cout<<"Please enter the category (Credit / Professor / Days / CourseName): ";
+            char category[50];
+            cin.getline(category,50);
+            // string courseName = courseNum.substr(0,2);
+            string courseName;
+            courseName.push_back(courseNum[0]);
+            courseName.push_back(courseNum[1]);
+            char combinedInput[100];
+            strcpy(combinedInput,courseNum);
+            strcat(combinedInput,",");
+            strcat(combinedInput,category);
+            strcat(combinedInput,"\0");
+            cout<<combinedInput<<endl;
+
+            if(courseName.compare("CS")==0){
+                if ((numbytes = sendto(sockfd, combinedInput, strlen(combinedInput), 0,
+                p->ai_addr, p->ai_addrlen)) == -1) {
+                perror("talker: sendto");
+                exit(1);
+                }
+                printf("talker: sent %d bytes to %s\n", numbytes, courseNum);
+
+                char buf[MAXBUFLEN];
+                struct sockaddr_storage their_addr;
+                socklen_t addr_len;
+                addr_len = sizeof their_addr;
+                if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+                    (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+                    perror("recvfrom");
+                    exit(1);
+                }
+                cout<<buf<<endl;
+                memset(buf, 0, sizeof buf);
+            }
+            else if(courseName.compare("EE")==0){
+                if ((numbytesEE = sendto(sockfdEE, combinedInput, strlen(combinedInput), 0,
+                pEE->ai_addr, pEE->ai_addrlen)) == -1) {
+                perror("talker: sendto");
+                exit(1);
+                }
+                printf("talker: sent %d bytes to %s\n", numbytesEE, courseNum);
+
+                char buf[MAXBUFLEN];
+                struct sockaddr_storage their_addr;
+                socklen_t addr_len;
+                addr_len = sizeof their_addr;
+                if ((numbytesEE = recvfrom(sockfdEE, buf, MAXBUFLEN-1 , 0,
+                    (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+                    perror("recvfrom");
+                    exit(1);
+                }
+                cout<<buf<<endl;
+                memset(buf, 0, sizeof buf);
+                
+            }
+            else{
+                cout<<"Wrong course input"<<endl;
+            }
+            //char encryptedInput[100];
+
+        }
+        
+
+
+
+
     }
 	
 
-	freeaddrinfo(servinfo);
-
 	
-	close(sockfd);
    
 }
